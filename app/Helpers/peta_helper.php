0bb7d7cca855707c4fd7c90030ea1eab8a57d11f
221910812 <221910812@stis.ac.id>
2022-05-03 23:34:57 +0700

    tambah peta umum, khusus, dan klasifikasi

--- app/Helpers/peta_helper.php
+++ app/Helpers/peta_helper.php
@@ -12,30 +12,35 @@ function getPeta()
         ],
         '2' => 
         [
+            'id' => '2',
             'nama' => 'Peta Umum Perubahan Lahan Purwakarta',
             'generate' => 'assets/peta_umum/purwakarta/js/peta_umum_purwakarta.js',
             'data' => 'assets/peta_umum/purwakarta/data/purwakarta.js',
         ],
         '3' => 
         [
+            'id' => '3',
             'nama' => 'Peta Khusus Perubahan Lahan Bandung Barat',
             'generate' => 'assets/peta_khusus/bandung_barat/js/peta_khusus_bandung_barat.js',
             'data' => 'assets/peta_khusus/bandung_barat/data/bandung_barat.js',
         ],
         '4' => 
         [
+            'id' => '4',
             'nama' => 'Peta Khusus Perubahan Lahan Purwakarta',
             'generate' => 'assets/peta_khusus/purwakarta/js/peta_khusus_purwakarta.js',
             'data' => 'assets/peta_khusus/purwakarta/data/purwakarta.js',
         ],
         '5' => 
         [
+            'id' => '5',
             'nama' => 'Peta Klasifikasi Tutupan Lahan 2021 Sentinel-2 Lokus Bandung Barat',
             'generate' => 'assets/peta_klasifikasi/bandung_barat/js/peta_klasifikasi_bandung_barat.js',
             'data' => 'assets/peta_klasifikasi/bandung_barat/data/bandung_barat.js',
         ],
         '6' => 
         [
+            'id' => '6',
             'nama' => 'Peta Klasifikasi Tutupan Lahan 2021 Sentinel-2 Lokus Purwakarta',
             'generate' => 'assets/peta_klasifikasi/purwakarta/js/peta_klasifikasi_purwakarta.js',
             'data' => 'assets/peta_klasifikasi/purwakarta/data/purwakarta.js',
