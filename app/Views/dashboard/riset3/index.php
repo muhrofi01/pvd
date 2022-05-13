57902275361d201c038c28690fc10e2d18841c44
221910812 <221910812@stis.ac.id>
2022-05-14 03:44:52 +0700

    penyesuaian halaman index dasbor tiap riset

--- app/Views/dashboard/riset3/index.php
+++ app/Views/dashboard/riset3/index.php
@@ -32,17 +32,18 @@
     <section class="content px-4 mt-4">
       <div class="container-fluid">
         <div class="row">
-          <div class="accordion w-100" id="accordionExample">
+          <div class="accordion w-100" id="accordionKotak1">
             <div class="card">
               <div class="card-header" id="headingOne">
                 <h2 class="mb-0">
-                  <button class="btn btn-link btn-block text-left w-100" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    Collapsible Group Item #1
+                  <button class="btn btn-link btn-block text-left d-flex justify-content-between align-items-center collapsed" type="button" onclick="putar(this)" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style="color:rgb(0, 75, 60); font-weight: bold;">
+                    Deskripsi Umum Hasil Penelitian Riset 3
+                  <i class="accordion-icon fas fa-chevron-circle-down"></i>
                   </button>
                 </h2>
               </div>
 
-              <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+              <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionKotak1">
                 <div class="card-body">
                   Some placeholder content for the first accordion panel. This panel is shown by default, thanks to the <code>.show</code> class.
                 </div>
@@ -51,12 +52,13 @@
             <div class="card">
               <div class="card-header" id="headingTwo">
                 <h2 class="mb-0">
-                  <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                    Collapsible Group Item #2
+                  <button class="btn btn-link btn-block text-left d-flex justify-content-between align-items-center collapsed" type="button" onclick="putar(this)" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" style="color:rgb(0, 75, 60); font-weight: bold;">
+                    Judul Kajian 1
+                    <i class="accordion-icon fas fa-chevron-circle-down"></i>
                   </button>
                 </h2>
               </div>
-              <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+              <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionKotak1">
                 <div class="card-body">
                   Some placeholder content for the second accordion panel. This panel is hidden by default.
                 </div>
@@ -65,12 +67,13 @@
             <div class="card">
               <div class="card-header" id="headingThree">
                 <h2 class="mb-0">
-                  <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                    Collapsible Group Item #3
+                  <button class="btn btn-link btn-block text-left d-flex justify-content-between align-items-center collapsed" type="button" onclick="putar(this)" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree" style="color:rgb(0, 75, 60); font-weight: bold;">
+                    Judul Kajian 2
+                    <i class="accordion-icon fas fa-chevron-circle-down"></i>
                   </button>
                 </h2>
               </div>
-              <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+              <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionKotak1">
                 <div class="card-body">
                   And lastly, the placeholder content for the third and final accordion panel. This panel is hidden by default.
                 </div>
@@ -88,6 +91,23 @@
 <?= $this->endSection() ?>
 
 <?= $this->section('js') ?>
-
+<script>
+    let putar = function(e) {
+      
+      if ($(e).children().hasClass('fa-rotate-180')) {
+        $(e).children().removeClass('fa-rotate-180');
+      } else {
+        if ($('#accordionKotak1 .card .card-header button').children().hasClass('fa-rotate-180') && 
+            $('#accordionKotak1 .card').find('div.collapse').hasClass('show')) {
+          $('#accordionKotak1.card .card-header button').children().removeClass('fa-rotate-180');
+        }
+        // if ($('#accordionTujuan3 .card .card-header button').children().hasClass('fa-rotate-180')&& 
+        //     $('#accordionTujuan3 .card').find('div.collapse').hasClass('show')) {
+        //   $('#accordionTujuan3 .card .card-header button').children().removeClass('fa-rotate-180');
+        // }
+        $(e).children().toggleClass('fa-rotate-180');
+      }
+    }
+</script>
 <?= $this->endSection() ?>
   
