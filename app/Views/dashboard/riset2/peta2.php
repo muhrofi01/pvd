8a7e5945761ea2cea554aa702f0bbfc1fe9ca738
221910812 <221910812@stis.ac.id>
2022-05-15 22:19:37 +0700

    buat template visualisasi peta kajian 2 anades 2

--- app/Views/dashboard/riset2/peta2.php
+++ app/Views/dashboard/riset2/peta2.php
@@ -1,6 +1,7 @@
 <?= $this->extend('dashboard/layout/main') ?>
 
 <?= $this->Section('css') ?>
+
 <style>
     #dropdown h1 {
       font-size: 1.5em;
@@ -39,7 +40,24 @@
         color: #0A5446; 
         font-weight:bold;
     }
+    .dropdown button.btn {
+        width: 289.08px;
+        height: 50px;
+        background: #0A5446;
+        border-radius: 10px 10px 0px 0px;
+        border: none;
+        font-size: 20px;
+        color: #ffffff; 
+        font-weight:bold;
+    }
+    #map,#petaLaju_bb, #petaLaju_pwk {
+      border: 5px solid #0A5446;
+    }
+    .dropdown .dropdown-item.active{
+      background-color: #0A5446;
+    }
   </style>
+  
 <?= $this->endSection() ?>
 
 <?= $this->Section('content') ?>
@@ -49,181 +67,37 @@
     <section class="content-header">
       <div class="container-fluid pl-4" style="color: rgb(0, 75, 60);">
         <div class="row mb-2 baris-judul-riset">
-          <div class="col-sm-6">
-            <h3 class="judul-riset">Karakteristik dan Faktor-Faktor yang Mempengaruhi Alih Fungsi Lahan Pertanian</h3>
+          <div class="col-sm-10">
+            <h3 class="judul-riset">Pemetaan Klasifikasi dan Alih Fungsi Lahan Pertanian dengan Citra Satelit</h3>
           </div>
         </div>
       </div><!-- /.container-fluid -->
     </section>
 
     <!-- Main content -->
-    <section class="content pl-4 mt-4">
-      <div class="container-fluid" id="app">
-            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link active" id="pills-tujuan2-tab" data-bs-toggle="pill" data-bs-target="#pills-tujuan2" type="button" role="tab" aria-controls="pills-tujuan2" aria-selected="true">Tujuan 2</button>
-                </li>
-                <li class="nav-item" role="presentation" style="margin-left:-7px; border-radius:10px;">
-                    <button class="nav-link" id="pills-tujuan3-tab" data-bs-toggle="pill" data-bs-target="#pills-tujuan3" type="button" role="tab" aria-controls="pills-tujuan3" aria-selected="false">Tujuan 3</button>
-                </li>
-            </ul>
-            <div class="tab-content" id="pills-tabContent">
-                <div class="tab-pane fade show active" id="pills-tujuan2" role="tabpanel" aria-labelledby="pills-tujuan2-tab">
-                    <div class="card-body p-0">
-                      <div class="d-md-flex">
-                        <div class="accordion w-100" id="accordionTujuan2">
-                          <div class="card">
-                            <div class="card-header" style="background-color: #0A5446;" id="heading1">
-                              <h2 class="mb-0">
-                                <button @click="togglePeta1" class="text-white btn btn-link btn-block text-left w-100 d-flex justify-content-between align-items-center" onclick="putar(this)" type="button" data-toggle="collapse" data-target="#collapse1" aria-expanded="true" aria-controls="collapse1">
-                                  Peta 1
-                                  <i class="accordion-icon fas fa-chevron-circle-down"></i>
-                                </button>
-                              </h2>
-                            </div>
-
-                            <div v-if="isVisible1" id="collapse1" class="collapse" aria-labelledby="heading1" data-parent="#accordionTujuan2">
-                              <div class="card-body">
-                              Map akan digenerate disini
-                              </div>
-                            </div>
-                          </div>
-                          <div class="card">
-                            <div class="card-header" style="background-color: #0A5446;" id="heading2">
-                              <h2 class="mb-0">
-                                <button @click="togglePeta2" class="text-white btn btn-link btn-block text-left d-flex justify-content-between align-items-center collapsed" onclick="putar(this)" type="button" data-toggle="collapse" data-target="#collapse2" aria-expanded="false" aria-controls="collapse2">
-                                  Peta 2
-                                  <i class="accordion-icon fas fa-chevron-circle-down"></i>
-                                </button>
-                              </h2>
-                            </div>
-                            <div v-if="isVisible2" id="collapse2" class="collapse" aria-labelledby="heading2" data-parent="#accordionTujuan2">
-                              <div class="card-body">
-                              Map akan digenerate disini
-                              </div>
-                            </div>
-                          </div>
-                          <div class="card">
-                            <div class="card-header" style="background-color: #0A5446;" id="heading3">
-                              <h2 class="mb-0">
-                                <button @click="togglePeta3" class="text-white btn btn-link btn-block text-left d-flex justify-content-between align-items-center collapsed" onclick="putar(this)" type="button" data-toggle="collapse" data-target="#collapse3" aria-expanded="false" aria-controls="collapse3">
-                                  Peta 3
-                                  <i class="accordion-icon fas fa-chevron-circle-down"></i>
-                                </button>
-                              </h2>
-                            </div>
-                            <div v-if="isVisible3" id="collapse3" class="collapse" aria-labelledby="heading3" data-parent="#accordionTujuan2">
-                              <div class="card-body">
-                              Map akan digenerate disini
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <!-- /.card-body -->
-                </div>
-                <div class="tab-pane fade show" id="pills-tujuan3" role="tabpanel" aria-labelledby="pills-tujuan3-tab">
-                    <div class="card-body p-0">
-                      <div class="d-md-flex">
-                        <div class="accordion w-100" id="accordionTujuan3">
-                          <div class="card">
-                            <div class="card-header" style="background-color: #0A5446;" id="headingOne">
-                              <h2 class="mb-0">
-                                <button @click="togglePeta4" class="text-white btn btn-link btn-block text-left d-flex justify-content-between align-items-center w-100" onclick="putar(this)" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                  Peta 1
-                                  <i class="accordion-icon fas fa-chevron-circle-down"></i>
-                                </button>
-                              </h2>
-                            </div>
-
-                            <div v-if="isVisible4" id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionTujuan3">
-                              <div class="card-body">
-                              Map akan digenerate disini
-                              </div>
-                            </div>
-                          </div>
-                          <div class="card">
-                            <div class="card-header" style="background-color: #0A5446;" id="headingTwo">
-                              <h2 class="mb-0">
-                                <button @click="togglePeta5" class="text-white btn btn-link btn-block text-left d-flex justify-content-between align-items-center collapsed" onclick="putar(this)" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                  Peta 2
-                                  <i class="accordion-icon fas fa-chevron-circle-down"></i>
-                                </button>
-                              </h2>
-                            </div>
-                            <div v-if="isVisible5" id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionTujuan3">
-                              <div class="card-body">
-                              Map akan digenerate disini
-                              </div>
-                            </div>
-                          </div>
-                          <div class="card">
-                            <div class="card-header" style="background-color: #0A5446;" id="headingThree">
-                              <h2 class="mb-0">
-                                <button @click="togglePeta6" class="text-white btn btn-link btn-block text-left d-flex justify-content-between align-items-center collapsed" onclick="putar(this)" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                  Peta 3
-                                  <i class="accordion-icon fas fa-chevron-circle-down"></i>
-                                </button>
-                              </h2>
-                            </div>
-                            <div v-if="isVisible6" id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionTujuan3">
-                              <div class="card-body">
-                              Map akan digenerate disini
-                              </div>
-                            </div>
-                          </div>
-                          <div class="card">
-                            <div class="card-header" style="background-color: #0A5446;" id="headingFour">
-                              <h2 class="mb-0">
-                                <button @click="togglePeta7" class="text-white btn btn-link btn-block text-left d-flex justify-content-between align-items-center collapsed" onclick="putar(this)" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                                  Peta 4
-                                  <i class="accordion-icon fas fa-chevron-circle-down"></i>
-                                </button>
-                              </h2>
-                            </div>
-                            <div v-if="isVisible7" id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordionTujuan3">
-                              <div class="card-body">
-                              Map akan digenerate disini
-                              </div>
-                            </div>
-                          </div>
-                          <div class="card">
-                            <div class="card-header" style="background-color: #0A5446;" id="headingFive">
-                              <h2 class="mb-0">
-                                <button @click="togglePeta8" class="text-white btn btn-link btn-block text-left d-flex justify-content-between align-items-center collapsed" onclick="putar(this)" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-                                  Peta 5
-                                  <i class="accordion-icon fas fa-chevron-circle-down"></i>
-                                </button>
-                              </h2>
-                            </div>
-                            <div v-if="isVisible8" id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordionTujuan3">
-                              <div class="card-body">
-                              Map akan digenerate disini
-                              </div>
-                            </div>
-                          </div>
-                          <div class="card">
-                            <div class="card-header" style="background-color: #0A5446;" id="headingSix">
-                              <h2 class="mb-0">
-                                <button @click="togglePeta9" class="text-white btn btn-link btn-block text-left d-flex justify-content-between align-items-center collapsed" onclick="putar(this)" type="button" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
-                                  Peta 6
-                                  <i class="accordion-icon fas fa-chevron-circle-down"></i>
-                                </button>
-                              </h2>
-                            </div>
-                            <div v-if="isVisible9" id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordionTujuan3">
-                              <div class="card-body">
-                              Map akan digenerate disini
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <!-- /.card-body -->
-                </div>
+    <section class="content pl-4">
+      <div class="container-fluid">
+        <div class="d-flex">
+          <div class="dropdown">
+            <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-expanded="false">
+              Tujuan 2
+            </button>
+            <div class="dropdown-menu" style="z-index: 10000000;" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="">Peta Tematik Laju Alih Fungsi Lahan</a>
+              <a class="dropdown-item" href="">Bivariate Choropleth 1</a>
+              <a class="dropdown-item" href="">Bivariate Choropleth 2</a>
+              <a class="dropdown-item" href="">Bivariate Choropleth 3</a>
             </div>
+          </div>
+        </div>
+        <div class="content-peta w-100 mt-4">
+          <?php if (isset($peta['nama'])) : ?>
+            <h2 class="text-center"><?= $peta['nama'] ?></h2>
+          <?php else : ?>
+            <h2 class="text-center">Peta Tematik Laju Alih Fungsi Lahan</h2>
+          <?php endif ?>
+          <div id="map" style="height: 600px;"></div>
+          </div>
       </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
@@ -232,23 +106,8 @@
 <?= $this->endSection() ?>
 
 <?= $this->Section('js') ?>
-<!-- REQUIRED SCRIPTS -->
 
-<!-- PAGE PLUGINS -->
-<!-- jQuery Mapael -->
-<script src="/assets/plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
-<script src="/assets/plugins/raphael/raphael.min.js"></script>
-<script src="/assets/plugins/jquery-mapael/jquery.mapael.min.js"></script>
-<script src="/assets/plugins/jquery-mapael/maps/usa_states.min.js"></script>
-<!-- ChartJS -->
-<script src="/assets/plugins/chart.js/Chart.min.js"></script>
-
-<!-- AdminLTE for demo purposes -->
-<script src="/assets/dist/js/demo.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="/assets/dist/js/pages/dashboard2.js"></script>
-
-<script>
+<!-- <script>
     let putar = function(e) {
       
       if ($(e).children().hasClass('fa-rotate-180')) {
@@ -265,108 +124,5 @@
         $(e).children().toggleClass('fa-rotate-180');
       }
     }
-</script>
-
-<script src="https://unpkg.com/vue@next"></script>
-<script>
-  let app = Vue.createApp({
-      data: function() {
-          return {
-              isVisible1: false,
-              isVisible2: false,
-              isVisible3: false,
-              isVisible4: false,
-              isVisible5: false,
-              isVisible6: false,
-              isVisible7: false,
-              isVisible8: false,
-              isVisible9: false,
-          }
-      },
-      methods: {
-          togglePeta1() {
-            for (let i = 1; i <= 9; i++) {
-                if (i != 1) {
-                  this['isVisible' + i] = false;
-                } else {
-                  this['isVisible' + i] = true;
-                }
-            }
-          },
-          togglePeta2() {
-            for (let i = 1; i <= 9; i++) {
-                  if (i != 2) {
-                    this['isVisible' + i] = false;
-                  } else {
-                    this['isVisible' + i] = true;
-                  }
-              }
-          },
-          togglePeta3() {
-            for (let i = 1; i <= 9; i++) {
-                  if (i != 3) {
-                    this['isVisible' + i] = false;
-                  } else {
-                    this['isVisible' + i] = true;
-                  }
-              }
-          },
-          togglePeta4() {
-            for (let i = 1; i <= 9; i++) {
-                  if (i != 4) {
-                    this['isVisible' + i] = false;
-                  } else {
-                    this['isVisible' + i] = true;
-                  }
-              }
-          },
-          togglePeta5() {
-            for (let i = 1; i <= 9; i++) {
-                  if (i != 5) {
-                    this['isVisible' + i] = false;
-                  } else {
-                    this['isVisible' + i] = true;
-                  }
-              }
-          },
-          togglePeta6() {
-            for (let i = 1; i <= 9; i++) {
-                  if (i != 6) {
-                    this['isVisible' + i] = false;
-                  } else {
-                    this['isVisible' + i] = true;
-                  }
-              }
-          },
-          togglePeta7() {
-            for (let i = 1; i <= 9; i++) {
-                  if (i != 7) {
-                    this['isVisible' + i] = false;
-                  } else {
-                    this['isVisible' + i] = true;
-                  }
-              }
-          },
-          togglePeta8() {
-            for (let i = 1; i <= 9; i++) {
-                  if (i != 8) {
-                    this['isVisible' + i] = false;
-                  } else {
-                    this['isVisible' + i] = true;
-                  }
-              }
-          },
-          togglePeta9() {
-            for (let i = 1; i <= 9; i++) {
-                  if (i != 9) {
-                    this['isVisible' + i] = false;
-                  } else {
-                    this['isVisible' + i] = true;
-                  }
-              }
-          },
-      }
-  })
-  app.mount('#app')
-</script>
+</script> -->
 <?= $this->endSection() ?>
